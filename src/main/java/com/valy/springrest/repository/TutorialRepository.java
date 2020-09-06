0f07405191f776a1363af7384e49a6c4ccfaa326
valymirauta <mirautav@gmail.com>
2020-09-06 22:52:29 +0300

    Spring Boot Rest App

--- src/main/java/com/valy/springrest/repository/TutorialRepository.java
+++ src/main/java/com/valy/springrest/repository/TutorialRepository.java
@@ -1,5 +1,13 @@
 package com.valy.springrest.repository;
 
-public class TutorialRepository {
+import com.valy.springrest.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
 }
